85cb949317ac095ccc55558a8c12b131a5daae90
Evgeniy-D-B <76701510+Evgeniy-D-B@users.noreply.github.com>
2022-03-28 03:20:47 +0300

    home-page.html added
    home-page.html added

---
+++ js/slick-int.js
@@ -0,0 +1,18 @@
+    $('.promotions-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    
+
+    $('.product-carousel').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    });
+
+    
+      
